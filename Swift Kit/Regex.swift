28eb1473a97f244b2270a124aa05e755ba7bf836
Aris Koxaras <aris@koxaras.com>
2016-10-24 11:14:59 +0300

    Added class to check for regular expressions in strings

---
+++ Swift Kit/Regex.swift
@@ -0,0 +1,25 @@
+//
+//  Regex.swift
+//  Swift Kit
+//
+//  Created by Aris Koxaras on 24/10/16.
+//  Copyright © 2016 Aris Koxaras. All rights reserved.
+//
+
+import Foundation
+
+struct Regex {
+    let internalExpression: NSRegularExpression
+    let pattern: String
+    
+    init(_ pattern: String) throws {
+        self.pattern = pattern
+        self.internalExpression = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+    }
+    
+    func test(input: String) -> Bool {
+        let range = NSMakeRange(0, (input as NSString).length)
+        
+        return self.internalExpression.matches(in: input, options: [], range: range).count > 0
+    }
+}
